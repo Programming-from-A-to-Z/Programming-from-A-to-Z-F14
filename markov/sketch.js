9eae847fa1f8fd26344818d4fc60e3e7c2a47162
Daniel Shiffman <daniel@shiffman.net>
2014-05-20 21:57:43 -0400

    ngram length word or char

--- markov/sketch.js
+++ markov/sketch.js
@@ -3,8 +3,17 @@
 function generate() {
   var output = document.getElementById('markov');
   var order = document.getElementById('order');
+  
   // No delimiter means do it by char
-  var generator = new MarkovGenerator(order.value,500,'');
+  var delimiter = '';
+  if(document.getElementById('word').checked) {
+    delimiter = ' ';
+  }
+
+  var len = Number(document.getElementById('length').value);
+  console.log(len);
+
+  var generator = new MarkovGenerator(order.value,len,delimiter);
   var input = document.getElementById('input');
   generator.feed(input.value);
   output.innerHTML = generator.generate();
