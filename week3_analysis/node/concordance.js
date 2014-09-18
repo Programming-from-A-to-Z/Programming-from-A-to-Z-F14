d36470df588247d81fd706f4bb245cabdf6a5828
Daniel Shiffman <daniel@shiffman.net>
2014-09-17 22:35:28 -0400

    first pass at concordance and tf-idf implementation

---
+++ week3_analysis/node/concordance.js
@@ -0,0 +1,94 @@
+// We can get command line arguments in a node program
+// Here we're checking to make sure we've typed three things (the last being the directory)
+if (process.argv.length < 3) {
+  console.log('Oops, you forgot to pass in a directory path.');
+  process.exit(1);
+}
+
+// // The 'fs' (file system) module allows us to read and write files
+// // http://nodejs.org/api/fs.html
+var fs = require('fs');
+// Now we are grabbing just a path
+var path = process.argv[2];
+// And we'll look at all files in that path
+var files = fs.readdirSync(path);
+
+// Read the file as utf8 and process the data
+// Notice how this is in a loop to parse all files
+for (var i = 0; i  < files.length; i++) {
+  fs.readFile(path+'/'+files[i], 'utf8', processFile);
+}
+
+// How many files have been read?
+var fileCount = 0;
+// An object that acts as dictionary of words and counts
+var wordcounts = {};
+
+function processFile(err, data) {
+  // If there's a problem
+  if (err) {
+    console.log('ooops, there was an error reading this file');
+    throw err;
+  }
+
+  // Split into array of tokens
+  var tokens = data.split(/\W+/);
+
+  // For every token
+  for (var i = 0; i < tokens.length; i++) {
+    // Lowercase everything to ignore case
+    var token = tokens[i].toLowerCase();
+    // Is this a new word?
+    if (wordcounts[token] == undefined) {
+      wordcounts[token] = 1;
+    // Otherwise just increment its count
+    } else {
+      wordcounts[token]++;
+    }
+  }
+
+  // This file finished
+  fileCount++;
+
+  // Is this the last file?
+  if (fileCount === files.length) {
+    finish();
+  }
+}
+
+
+// Once we've hit the last file
+function finish() {
+
+  // This is a somewhat uncessary step but here's how
+  // we could sort the concordance by counts
+  
+  // An array of sorted objects
+  var sorted = [];
+
+  // For every key in the dictionary
+  for (key in wordcounts) {
+    // Make sure it's really a key and is not empty
+    if (wordcounts.hasOwnProperty(key) && key.length > 0) {
+      // Add it to the array
+      sorted.push({'word': key, 'count': wordcounts[key]});
+    }
+  }
+
+  // A fancy way to sort each element
+  // Compare the counts
+  sorted.sort(function(a,b) {
+    return (b.count - a.count);
+  });
+  
+  
+  // Just spit out the top 15 words
+  var max = 15;
+  if (sorted.length < max) {
+    max = sorted.length;
+  }
+  for (var i = 0; i < max; i++) {
+     console.log(sorted[i].word + ': ' + sorted[i].count);
+  }
+}
+
---
+++ week3_analysis/node/tf-idf.js
@@ -0,0 +1,150 @@
+// Daniel Shiffman
+// Programming from A to Z, Fall 2014
+// https://github.com/shiffman/Programming-from-A-to-Z-F14
+
+// TF-IDF analysis
+// http://en.wikipedia.org/wiki/Tf%E2%80%93idf
+
+// We can get command line arguments in a node program
+if (process.argv.length < 4) {
+  console.log('Oops, you forgot to pass in a directory path and file to evaluate.');
+  process.exit(1);
+}
+
+// The 'fs' (file system) module allows us to read and write files
+// http://nodejs.org/api/fs.html
+var fs = require('fs');
+// A path for all the files in the world
+var path = process.argv[2];
+// The specific file to perform tf-idf on
+var file = process.argv[3];
+// And we'll look at all files in that path
+var files = fs.readdirSync(path);
+
+// An object that acts as dictionary of words
+// With each word we'll store an object that includes
+// Term frequency
+// Inverse document frequency
+// tf-idf score
+var dictionary = {};
+
+// Let's use an extra array just to track all the keys
+// We can sort this array as needed
+var keys = [];
+
+// Read the main file we want to evaluate
+fs.readFile(file, 'utf-8', countWords);
+
+// Count the words of just that one file
+function countWords(err, data) {
+  // If there's a problem
+  if (err) {
+    console.log('ooops, there was an error reading this file');
+    throw err;
+  }
+
+  // Split into array of tokens
+  var tokens = data.split(/\W+/);
+
+  // For every token
+  for (var i = 0; i < tokens.length; i++) {
+    // Lowercase everything to ignore case
+    var token = tokens[i].toLowerCase();
+    if (token.length > 0) {
+      // Is this a new word?
+      if (dictionary[token] === undefined) {
+        dictionary[token] = {};
+        // The word itself
+        dictionary[token].word = token;
+        // Count of 1
+        dictionary[token].count = 1;
+        // This file will get counted again later
+        dictionary[token].docCount = 0;
+        // Add a key
+        keys.push(token);
+      // Otherwise just increment its count
+      } else {
+        dictionary[token].count++;
+      }
+    }
+  }
+}
+
+// Now we have to look through every file and determine
+// the document frequency
+var fileCount = 0;
+for (var i = 0; i  < files.length; i++) {
+  fs.readFile(path+'/'+files[i], 'utf8', countDocuments);
+}
+
+// Read all docs
+function countDocuments(err, data) {
+  // If there's a problem
+  if (err) {
+    console.log('ooops, there was an error reading this file');
+    throw err;
+  }
+  
+  // Split into words
+  var tokens = data.split(/\W+/);
+  
+  // A temporary dictionary of words in this document
+  var tempDict = {};
+
+  // For every token
+  for (var i = 0; i < tokens.length; i++) {
+    // Lowercase everything to ignore case
+    var token = tokens[i].toLowerCase();
+    // Simpler we just need to see if it exists or not
+    if (tempDict[token] === undefined) {
+      tempDict[token] = true;
+    }
+  }
+
+
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    // Does this word exist in this document?
+    if (tempDict[key]) {
+      dictionary[key].docCount++;
+    }
+  }
+
+  fileCount++;
+  // Done with the files, finish
+  if (fileCount === files.length) {
+    finish();
+  }
+}
+
+// Once we've hit the last file
+function finish() {
+  // calculate tf-idf score
+  for (var i = 0; i < keys.length; i++) {
+    var key = keys[i];
+    var word = dictionary[key];
+    var tf = word.count / keys.length;
+    // See: 
+    var idf = Math.log(files.length / word.docCount);
+    word.tfidf = tf * idf;
+  }
+
+
+  // A fancy way to sort each element
+  // Sort the keys but the tfidf score in the dictionary
+  keys.sort(function(a,b) {
+    return (dictionary[b].tfidf - dictionary[a].tfidf);
+  });
+  
+  
+  // Just spit out the top 15 ones
+  var max = 15;
+  if (keys.length < max) {
+    max = keys.length;
+  }
+  for (var i = 0; i < max; i++) {
+    var key = keys[i];
+    var score = dictionary[key].tfidf.toPrecision(2);
+    console.log(key + ': ' + score);
+  }
+}
