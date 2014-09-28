26b383669c83240164bd6a6a8444bcbe74812bf1
Daniel Shiffman <daniel@shiffman.net>
2014-09-28 17:27:42 +0100

    refactor markov example for week 4

---
+++ week4_generate/markov/sketch.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  noCanvas();
+}
+
+function draw() {
+
+}
+
+function generate() {
+  var output = getElement('markov');
+  var order = getElement('order');
+  var byWord = getElement('word');
+  var length = getElement('length');
+  
+  var generator = new MarkovGenerator(order.value(), length.value(), byWord.elt.checked);
+  var input = getElement('input');
+  generator.feed(input.value());
+  output.html(generator.generate());
+}
