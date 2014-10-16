f3b2425fa83e90e25d794c86b359fb7443f85ddc
Daniel Shiffman <daniel@shiffman.net>
2014-10-16 17:04:20 -0400

    now with bind

--- week6_apis/servi_basics/07_concordance_database/server.js
+++ week6_apis/servi_basics/07_concordance_database/server.js
@@ -10,22 +10,41 @@ var concordance = useDatabase('words');
 route('/save', saveData);
 
 
-function searchIt(word) {
-  return function(data) {
-    if (data.length === 0) {
-      var record = {};
-      record.word = word;
-      record.count = 1;
-      concordance.add(record);
-      console.log('New word! ' + word + ': ' + 1);
-    } else {
-      var record = data[0]; // Assume it's the first record
-      var num = data[0].count + 1;
-      var id = data[0]._id;
-      // concordance.update({_id: id}, {$set: {count: num}}, {});  
-      console.log('New count for ' + word + ': ' + num);
-      // concordance.change(id, {count: num});
-    }
+// function searchIt(word) {
+//   return function(data) {
+//     if (data.length === 0) {
+//       var record = {};
+//       record.word = word;
+//       record.count = 1;
+//       concordance.add(record);
+//       console.log('New word! ' + word + ': ' + 1);
+//     } else {
+//       var record = data[0]; // Assume it's the first record
+//       var num = data[0].count + 1;
+//       var id = data[0]._id;
+//       // concordance.update({_id: id}, {$set: {count: num}}, {});  
+//       console.log('New count for ' + word + ': ' + num);
+//       // concordance.change(id, {count: num});
+//     }
+//   }
+// }
+
+function searchIt(word, data) {
+  //console.log(data);
+  //console.log('what: ' + what);
+  if (data.length === 0) {
+    var record = {};
+    record.word = word;
+    record.count = 1;
+    concordance.add(record);
+    console.log('New word! ' + word + ': ' + 1);
+  } else {
+    var record = data[0]; // Assume it's the first record
+    var num = data[0].count + 1;
+    var id = data[0]._id;
+    // concordance.update({_id: id}, {$set: {count: num}}, {});  
+    console.log('New count for ' + word + ': ' + num);
+    // concordance.change(id, {count: num});
   }
 }
 
@@ -39,8 +58,8 @@ function saveData(request) {
   var txt = request.fields.txt;
   var tokens = txt.split(/\W+/);  
   for (var i = 0; i < tokens.length; i++) {
-    var word = tokens[i]; 
-    concordance.search('word', tokens[i], searchIt(tokens[i])); 
+    concordance.search('word', tokens[i], searchIt.bind(null,tokens[i])); 
+    // concordance.search('word', tokens[i], searchIt(tokens[i])); 
   }
   //request.respond('finished');
   request.redirect('/');
